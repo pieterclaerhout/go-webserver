72c458e5f89fba6233e1b3620a1c79dcfb3692e1
Pieter Claerhout <pieter@yellowduck.be>
2019-10-11 15:19:40 +0200

    Reordered the functions

--- module.go
+++ module.go
@@ -6,7 +6,7 @@ import (
 
 // Module defines a server module
 type Module interface {
-	Start()                     // Executed when the server starts
 	Register(router *echo.Echo) // The function which registers the endpoints on the router
+	Start()                     // Executed when the server starts
 	Stop()                      // Executed when the server stops
 }
